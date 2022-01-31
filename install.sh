2b96e3e99f864fff9d211338961dafe5ce1a5b6b
Josh Anderson <andersonshatch@gmail.com>
2022-01-31 14:11:36 +0000

    Update brew install call

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cp dotfiles/.vimrc ~/
 
 if test ! $(which brew)
 then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle -v
