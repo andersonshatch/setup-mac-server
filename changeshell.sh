f3d0bbacb97a6a39b5ee51c0a3fabc5c9b6b3d4b
Josh Anderson <andersonshatch@gmail.com>
2022-01-31 14:23:33 +0000

    Update bash path

--- changeshell.sh
+++ changeshell.sh
@@ -1,5 +1,5 @@
 # Change shell to homebrew bash
-NEWSHELL='/usr/local/bin/bash'
+NEWSHELL='/opt/homebrew/bin/bash'
 if [ $SHELL != $NEWSHELL ] ; then
 	echo "Changing shell to $NEWSHELL..."
 	if ! grep $NEWSHELL /etc/shells > /dev/null 2>&1 ; then
